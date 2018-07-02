e251933839ee06174dc2054357a841700746ca96
llamafarmer <joshsisto@gmail.com>
2018-07-02 16:26:01 -0700

    changed test
    messing with dirs

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,6 +1,6 @@
 import pytest
 
-from Pi_Weather_Station.weather import set_screen_color
+from code.weather import set_screen_color
 
 def test_set_screen_color():
     assert set_screen_color(25) == [0, 0, 155]
