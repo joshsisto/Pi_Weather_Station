b6bfb61bf865d7fc6b7d9fd6e7be56a0bc0ad168
llamafarmer <joshsisto@gmail.com>
2018-07-07 08:58:35 -0700

    Using Dictionary to Unpack
    Based on feedback from /u/Rage_Boner
    
    https://www.reddit.com/r/learnpython/comments/8vod7q/revisiting_an_old_project_8_months_later/

--- src/web_app.py
+++ src/web_app.py
@@ -10,17 +10,18 @@ def index():
     sense = SenseHat()
     sense.clear()
 
-    celcius = round(sense.get_temperature(), 1)
-    fahrenheit = round(1.8 * celcius + 32, 1)
-    humidity = round(sense.get_humidity(), 1)
-    pressure = round(sense.get_pressure(), 1)
-
     acceleration = sense.get_accelerometer_raw()
-    x = round(acceleration['x'], 2)
-    y = round(acceleration['y'], 2)
-    z = round(acceleration['z'], 2)
-
-    return render_template('weather.html', celcius=celcius, fahrenheit=fahrenheit, humidity=humidity, pressure=pressure, x=x, y=y, z=z)
+    celcius      = round(sense.get_temperature(), 1)
+    kwargs = dict(
+        celcius     = celcius,
+        fahrenheit  = round(1.8 * celcius + 32, 1),
+        humidity    = round(sense.get_humidity(), 1),
+        pressure    = round(sense.get_pressure(), 1),
+        x = round(acceleration['x'], 2),
+        y = round(acceleration['y'], 2),
+        z = round(acceleration['z'], 2),
+    )
+    return render_template('weather.html', **kwargs)
 
 while __name__ == '__main__':
     app.run(host='0.0.0.0')
