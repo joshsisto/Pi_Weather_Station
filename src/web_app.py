3fdec2d0d539c57a4a87cad97603fc07bf2b65be
Vlad <pckltr@users.noreply.github.com>
2020-01-28 01:45:38 +0200

    Correct celsius typo

--- src/web_app.py
+++ src/web_app.py
@@ -14,10 +14,10 @@ def index():
     sense.clear()
 
     acceleration = sense.get_accelerometer_raw()
-    celcius      = round(sense.get_temperature(), 1)
+    celsius      = round(sense.get_temperature(), 1)
     kwargs = dict(
-        celcius     = celcius,
-        fahrenheit  = round(1.8 * celcius + 32, 1),
+        celsius     = celsius,
+        fahrenheit  = round(1.8 * celsius + 32, 1),
         humidity    = round(sense.get_humidity(), 1),
         pressure    = round(sense.get_pressure(), 1),
         x = round(acceleration['x'], 2),
