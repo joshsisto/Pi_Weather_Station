7f712d5682cc2d303052473abfc597a9a13e2e11
Josh Sisto <joshsisto@gmail.com>
2020-07-23 20:34:05 -0700

    added text alerts

---
+++ src/sendText.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from credentials import mailjet_user, mailjet_pass
+
+
+def send_email(message_body):
+    fromaddr = "joshsisto@gmail.com"  # from email address
+    toaddr = alert_addr  # destination email address
+    smtp_user = gmail_user
+    smtp_pass = gmail_app_pass
+    # print('smtp_pass' + smtp_pass)
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    # msg['Subject'] = subject  # subject
+
+    body = message_body  # body
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)  # e.g. ('in-v3.mailjet.com', 587)
+    server.starttls()
+    server.login(smtp_user, smtp_pass)
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
---
+++ src/window_alerts.py
@@ -0,0 +1,71 @@
+import datetime
+import time
+import csv
+import os
+import ast
+import glob
+from math import log
+from sense_hat import SenseHat
+from weather import get_timestamp
+from sendText import *
+
+
+def get_csv_data():
+    """Open the daily csv log and return the content"""
+    global csv_path
+    csv_list = []
+    day = get_timestamp().split()[0]
+    csv_path = os.path.join(os.path.dirname(__file__) + '/logs/', day + '.csv')
+    # csv_path = '/home/pi/Pi_Weather_Station/src/logs/' + day + '.csv'
+    with open(csv_path, 'r') as csv_file:
+        # content = f.read()
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            # print(row)
+            csv_list.append(row)
+    return csv_list
+
+get_csv_data()
+
+def get_dark_sky():
+    """Read the most recent dark sky log and return a list of the stats"""
+    csv_content = get_csv_data()
+    most_recent = csv_content[-1]
+    dark_sky_string = most_recent[9]
+    dark_sky_list = dark_sky_string.strip('][').split(', ')
+    ds_temp = dark_sky_list[0]
+    ds_cond = dark_sky_list[1].strip("'")
+    ds_fore = dark_sky_list[2].strip("'")
+    return [ds_temp, ds_cond, ds_fore]
+
+# print(os.path.basename(csv_path))
+# print(csv_path[:-3] + 'alert')
+
+
+
+def check_min():
+    try:
+        # alert_cont = read_alert()
+        minimum_temp = 74
+        current_temp = get_dark_sky()[0]
+        current_temp = float(current_temp)            
+        if current_temp <= minimum_temp:
+            print('It is 74 degrees or cooler! Time to open the windows')
+            return True
+        else:
+            print('Temperature is within limit set')
+            return False
+    except:
+        print('That did not work.')
+        print('probably did not have a value set for minimum temp')
+
+alert_file_path = csv_path[:-3] + 'alert'
+
+if os.path.exists(alert_file_path) == False:
+    print('no alert file detected')
+    if check_min() == True:
+        print("Temp reached! creating alert flag")
+        alert_flag = open(alert_file_path, 'w+')
+        print("Sending Text")
+        send_email('It is 74 degrees or cooler! Time to open the windows')
+
